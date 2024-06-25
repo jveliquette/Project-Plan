926c02f71ce379f2ea66f5ae90c7db65d4749d92
jveliquette <juliaveliquette@gmail.com>
2024-06-25 14:18:44 -0700

    Feature 5 Complete

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from projects.models import Project
 
 # Create your views here.
+def list_projects(request):
+    project_list = Project.objects.filter(owner_id=request.user.id)
+    context = {
+        "project_list": project_list,
+    }
+    return render(request, "projects/list_projects.html", context)
