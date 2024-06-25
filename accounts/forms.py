d8313620d72f7572b818dacd265f2f4faaf5ebfc
jveliquette <juliaveliquette@gmail.com>
2024-06-25 14:54:07 -0700

    Feature 7 complete

---
+++ accounts/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+class LogInForm(forms.Form):
+    username = forms.CharField(max_length=150)
+    password = forms.CharField(
+        max_length=150,
+        widget=forms.PasswordInput,
+    )
--- tracker/urls.py
+++ tracker/urls.py
@@ -26,4 +26,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", redirect_to_projects_list, name="home"),
     path("projects/", include("projects.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
