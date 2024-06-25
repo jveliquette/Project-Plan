32f896c58e4d06080dfb69396d41501a92c9beff
jveliquette <juliaveliquette@gmail.com>
2024-06-25 15:18:00 -0700

    Feature 10 complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
-from accounts.forms import LogInForm
+from accounts.forms import LogInForm, SignUpForm
 
 # Create your views here.
 def user_login(request):
@@ -29,3 +29,28 @@ def user_login(request):
 def user_logout(request):
     logout(request)
     return redirect("login")
+
+def signup(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            password_confirmation = form.cleaned_data['password_confirmation']
+            if password == password_confirmation:
+                # create new user with those values and save it to a variable
+                user = User.objects.create_user(
+                    username,
+                    password=password
+                )
+                # login user that was just created
+                login(request, user)
+                return redirect("list_projects")
+            else:
+                form.add_error("password", "the passwords do not match")
+    else:
+        form = SignUpForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "accounts/signup.html", context)
