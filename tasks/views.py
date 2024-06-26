b31a8acc3d7ca5628c30ee989f6516fe3a0d41e4
jveliquette <juliaveliquette@gmail.com>
2024-06-26 10:27:40 -0700

    Feature 16 complete

--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from tasks.forms import CreateTaskForm
+from tasks.models import Task
 
 # Create your views here.
 
@@ -17,3 +18,11 @@ def create_task(request):
         "form": form,
     }
     return render(request, "tasks/create_task.html", context)
+
+@login_required
+def show_my_tasks(request):
+    tasks = Task.objects.filter(assignee=request.user)
+    context = {
+        "my_tasks": tasks,
+    }
+    return render(request, "tasks/show_my_tasks.html", context)
