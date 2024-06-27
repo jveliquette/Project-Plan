3c2b91bf922a1075095222fc09e99137946c1018
jveliquette <juliaveliquette@gmail.com>
2024-06-27 16:11:50 -0700

    added edit task functionality

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from tasks.views import create_task, show_my_tasks, task_detail, add_notes
+from tasks.views import create_task, show_my_tasks, task_detail, add_notes, edit_task
 
 urlpatterns = [
     path("create/", create_task, name="create_task"),
     path("mine/", show_my_tasks, name="show_my_tasks"),
     path("<int:task_id>/", task_detail, name="task_detail"),
-    path("<int:task_id>/addnotes/", add_notes, name="add_notes")
+    path("<int:task_id>/add-notes/", add_notes, name="add_notes"),
+    path("<int:task_id>edit-task", edit_task, name="edit_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -54,3 +54,19 @@ def add_notes(request, task_id):
         "task": task,
     }
     return render(request, "tasks/add_notes.html", context)
+
+@login_required
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, assignee=request.user)
+    if request.method == "POST":
+        form = CreateTaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("task_detail", task_id=task.id)
+    else:
+        form = CreateTaskForm(instance=task)
+    context = {
+        "form": form,
+        "task": task,
+    }
+    return render(request, "tasks/edit_task.html", context)
