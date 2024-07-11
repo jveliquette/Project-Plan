4d8f7b82c17f1914a35f56503ed38d7b5a678875
jveliquette <juliaveliquette@gmail.com>
2024-07-11 13:00:09 -0700

    moved task cart to show_my_tasks page

--- tasks/views.py
+++ tasks/views.py
@@ -29,71 +29,6 @@ def create_task(request):
 @login_required
 def show_my_tasks(request):
     tasks = Task.objects.all()
-    context = {
-        "my_tasks": tasks,
-    }
-    return render(request, "tasks/show_my_tasks.html", context)
-
-
-@login_required
-def task_detail(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
-    project = task.project
-    context = {
-        "task": task,
-        "project": project,
-    }
-    return render(request, "tasks/task_detail.html", context)
-
-
-@login_required
-def add_notes(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
-    if request.method == "POST":
-        form = TaskNotesForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect("task_detail", task_id=task.id)
-    else:
-        form = TaskNotesForm(instance=task)
-    context = {
-        "form": form,
-        "task": task,
-    }
-    return render(request, "tasks/add_notes.html", context)
-
-@login_required
-def edit_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
-    if request.method == "POST":
-        form = EditTaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect("task_detail", task_id=task.id)
-    else:
-        form = EditTaskForm(instance=task)
-    context = {
-        "form": form,
-        "task": task,
-    }
-    return render(request, "tasks/edit_task.html", context)
-
-
-@login_required
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, id=task_id)
-    if request.method == "POST":
-        task.delete()
-        return redirect("show_my_tasks")
-    context = {
-        "task": task,
-    }
-    return render(request, "tasks/delete_task.html", context)
-
-
-@login_required
-def show_task_chart(request):
-    tasks = Task.objects.all()
     projects_data = []
     for task in tasks:
         project_name = f"{task.project.name}"
@@ -162,8 +97,141 @@ def show_task_chart(request):
         gantt_plot = "No data available to display the chart."
 
     context = {
-        "tasks": tasks,
+        "my_tasks": tasks,
         "plot_div": gantt_plot,
     }
+    return render(request, "tasks/show_my_tasks.html", context)
 
-    return render(request, 'tasks/show_task_chart.html', context)
+
+@login_required
+def task_detail(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    project = task.project
+    context = {
+        "task": task,
+        "project": project,
+    }
+    return render(request, "tasks/task_detail.html", context)
+
+
+@login_required
+def add_notes(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == "POST":
+        form = TaskNotesForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("task_detail", task_id=task.id)
+    else:
+        form = TaskNotesForm(instance=task)
+    context = {
+        "form": form,
+        "task": task,
+    }
+    return render(request, "tasks/add_notes.html", context)
+
+@login_required
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == "POST":
+        form = EditTaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+            return redirect("task_detail", task_id=task.id)
+    else:
+        form = EditTaskForm(instance=task)
+    context = {
+        "form": form,
+        "task": task,
+    }
+    return render(request, "tasks/edit_task.html", context)
+
+
+@login_required
+def delete_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == "POST":
+        task.delete()
+        return redirect("show_my_tasks")
+    context = {
+        "task": task,
+    }
+    return render(request, "tasks/delete_task.html", context)
+
+
+# @login_required
+# def show_task_chart(request):
+#     tasks = Task.objects.all()
+#     projects_data = []
+#     for task in tasks:
+#         project_name = f"{task.project.name}"
+#         projects_data.append({
+#             "Task": task.name,
+#             "Project": project_name,
+#             "Start": task.start_date,
+#             "Finish": task.due_date,
+#             "Completed": task.is_completed,
+#         })
+
+#     df = pd.DataFrame(projects_data)
+#     if not df.empty:
+#         projects = Project.objects.all()
+#         color_map = px.colors.qualitative.Dark24[:len(projects)]
+#         project_color_map = {project.name: color_map[i % len(color_map)] for i, project in enumerate(projects)}
+#         df["Color"] = df.apply(lambda row: "#FFFFFF" if row["Completed"] else project_color_map.get(row["Project"], "#FFFFFF"), axis=1)
+
+#         fig = px.timeline(
+#             df, x_start="Start", x_end="Finish", y="Task", color="Project",
+#             labels={"Task": "Task", "Start": "Start Date", "Finish": "Due Date"},
+#             color_discrete_map=project_color_map,
+#             template="plotly_dark",
+#             opacity=0.95,
+#         )
+#         fig.update_yaxes(autorange="reversed")
+
+#         completed_tasks = df[df["Completed"]]
+#         fig.add_trace(go.Scatter(
+#             x=completed_tasks["Start"],
+#             y=completed_tasks["Task"],
+#             mode="markers",
+#             marker=dict(symbol="square", size=10, color="#FFFFFF", line=dict(width=3, color="Black")),
+#             showlegend=True,
+#             name="Completed Tasks",
+#             text="Completed",
+#             hoverinfo="text",
+#             opacity=0.8,
+#             hoverlabel=dict(
+#                 bgcolor="white",
+#                 font=dict(color="black")
+#             )
+#         ))
+
+#         fig.update_layout(
+#             xaxis_title="Date",
+#             yaxis_title="Task",
+#             plot_bgcolor='rgba(0,0,0,0)',
+#             paper_bgcolor='rgba(0,0,0,0)',
+#             font=dict(color="white"),
+#             legend=dict(
+#                 title="Project",
+#                 font=dict(
+#                     family="Roboto, sans-serif",
+#                     size=14,
+#                     color="white"
+#                 )
+#             )
+#         )
+
+#         fig.update_xaxes(showgrid=True, gridcolor='rgba(255,255,255,0.2)')
+#         fig.update_yaxes(showgrid=True, gridcolor='rgba(255,255,255,0.2)')
+
+#         gantt_plot = fig.to_html(full_html=False, include_plotlyjs=False)
+#     else:
+#         gantt_plot = "No data available to display the chart."
+
+#     context = {
+#         "tasks": tasks,
+#         "plot_div": gantt_plot,
+#     }
+
+#     return render(request, 'tasks/show_task_chart.html', context)
