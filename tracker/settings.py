8fe9076cbaa604a403017460f5c7e062f65632d1
jveliquette <juliaveliquette@gmail.com>
2024-09-26 17:59:54 -0700

    settings to allowed hosts

--- tracker/settings.py
+++ tracker/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['project-plan-7087848d1390.herokuapp.com']
 
 
 # Application definition
